dbfdcd2a7f23b1a8731ea6c4d742777f17bd340a
Lucas Costa <luchtml5@gmail.com>
2023-02-21 15:01:17 -0300

    login page

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import './signIn.css';
+import logo from '../../assets/logo.png';
+import { Link } from 'react-router-dom';
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+  const { email, setEmail } = React.useState('');
+  const { password, setPassword } = React.useState('');
+
+  return (
+    <div className="container-center">
+      <div className="login">
+        <div className="login-area">
+          <img src={logo} alt="logo da pagina" />
+        </div>
+
+        <form>
+          <h1>Entrar</h1>
+          <input
+            type="text"
+            placeholder="email@emai.com"
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+          />
+          <input
+            type="password"
+            placeholder="**********"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+          <button type="submit">Acessar</button>
+        </form>
+
+        <Link to="/register">Criar uma conta</Link>
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
